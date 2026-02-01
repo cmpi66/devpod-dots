911624f5d140fab48b5b135796b95f2513442ae1
chris <munozchris@posteo.net>
2026-02-01 15:52:44 -0500

    addind install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+cd "$DOTFILES_DIR"
+
+PKGS=(
+  neovim
+)
+
+for pkg in "${PKGS[@]}"; do
+  echo "Stowing $pkg"
+  stow --target="$HOME" "$pkg"
+done
