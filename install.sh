8ceec825102c3f4e32b04483b405aabdcce4a20a
chris <munozchris@posteo.net>
2026-02-02 16:44:16 -0500

    updates

--- install.sh
+++ install.sh
@@ -6,6 +6,20 @@ if [ -f "$HOME/.zshrc" ]; then
   rm -f "$HOME/.zshrc"
 fi
 
+# Ensure zsh config dir exists
+mkdir -p "$HOME/.config/zsh"
+
+# install zap
+if [ ! -d "$HOME/.local/share/zap" ]; then
+  zsh <(curl -fsSL https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) \
+    --branch release-v1
+fi
+
+# Remove again
+if [ -d "$HOME/.config/zsh" ]; then
+  rm -rf "$HOME/.config/zsh"
+fi
+
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$DOTFILES_DIR"
 
