6aa7fc0809674c90cf15a27c9040ab0578085035
chris <munozchris@posteo.net>
2026-02-02 16:14:16 -0500

    updates

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Remove default zshrc if it exists
+if [ -f "$HOME/.zshrc" ]; then
+  rm -f "$HOME/.zshrc"
+fi
+
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$DOTFILES_DIR"
 
 PKGS=(
   neovim
+  shell
+  starship
+  zsh
 )
 
 for pkg in "${PKGS[@]}"; do
