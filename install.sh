7621a6539b5073ecd6b5c5b3a542ac7264e86560
chris <munozchris@posteo.net>
2026-02-03 15:01:37 -0500

    updating zap

--- install.sh
+++ install.sh
@@ -6,20 +6,6 @@ if [ -f "$HOME/.zshrc" ]; then
   rm -f "$HOME/.zshrc"
 fi
 
-# # Ensure zsh config dir exists
-# mkdir -p "$HOME/.config/zsh"
-
-# # install zap
-# if [ ! -d "$HOME/.local/share/zap" ]; then
-#   zsh <(curl -fsSL https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) \
-#     --branch release-v1
-# fi
-#
-# # Remove again
-# if [ -d "$HOME/.config/zsh" ]; then
-#   rm -rf "$HOME/.config/zsh"
-# fi
-
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$DOTFILES_DIR"
 
@@ -35,8 +21,8 @@ for pkg in "${PKGS[@]}"; do
   stow --target="$HOME" "$pkg"
 done
 
-# install zap
+# install zap and keep our zshrc
 if [ ! -d "$HOME/.local/share/zap" ]; then
   zsh <(curl -fsSL https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) \
-    --branch release-v1
+    --branch release-v1 --keep
 fi
